5f695fdcc25051941c9055c9b4fe089715df266e
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-01-09 13:40:37 +0100

    id pinecone and category added

---
+++ database/migrations/2025_01_09_112622_add_vectors_and_category_to_profiles_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->string('id_pinecone')->nullable()->after('experiences'); // Ajoute un champ JSON pour les vecteurs
+            $table->string('category')->nullable()->after('vectors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+        });
+    }
+};

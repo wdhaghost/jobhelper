225fcd514cdf143e9784365c93b73d2623bd09c1
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2025-01-09 11:24:34 +0100

    profiles routes created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('profiles')->group(function () {
+    Route::get('/', [ProfileController::class, 'index'])->name('profiles.index'); // Liste des profils
+    Route::get('/create', [ProfileController::class, 'create'])->name('profiles.create'); // Formulaire de création
+    Route::post('/', [ProfileController::class, 'store'])->name('profiles.store'); // Enregistrement du profil
+    Route::get('/{profile}', [ProfileController::class, 'show'])->name('profiles.show'); // Détails d'un profil
+    Route::get('/{profile}/edit', [ProfileController::class, 'edit'])->name('profiles.edit'); // Formulaire d'édition
+    Route::put('/{profile}', [ProfileController::class, 'update'])->name('profiles.update'); // Mise à jour du profil
+    Route::delete('/{profile}', [ProfileController::class, 'destroy'])->name('profiles.destroy'); // Suppression d'un profil
 });
